42d733f7d93117aad6f4f4b6cf8ddb23f1c352f3
devanshu <devanshukumar45@gmail.com>
2021-10-21 22:02:04 +0530

    created order gen routes

---
+++ routes/payment_routes.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const router = require('express').Router()
+const Razorpay = require('razorpay')
+
+// PAYMENT ROUTE
+router.post('/orders', async (req, res)=>{
+    var options = {
+        amount: 50000,
+        currency: "INR",
+        receipt: "order_rcptid_11"
+    }
+    const instance = new Razorpay({
+        key_id: process.env.RAZORPAY_KEY_ID,
+        key_secret: process.env.RAZORPAY_KEY_SECRET
+    })
+    instance.orders.create(options, (err, order)=>{
+        res.json(order)
+        console.log(order)
+    })
+
+})
+
+module.exports = router
