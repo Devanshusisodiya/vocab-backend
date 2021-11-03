04228e129bfa9c7df19b4a8e5695f058fe748136
devanshu <devanshukumar45@gmail.com>
2021-11-03 15:06:06 +0530

    edited word model, configed routes

--- models/word.js
+++ models/word.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const Word = mongoose.Schema({
     word: {type: String,required: true},
-    meaning: {type: String,required: true}
+    meaning: {type: String,required: true},
+    images: {type: Array, required: true},
+    video :{type: String, required: true}
 })
 // ADD ASSETS ARRAY
 
